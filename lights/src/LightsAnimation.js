c8cbdb9405f999d88afe845c969b4d0a7f3a1603
Vladislav Guzenkov <vladislav.srg@yandex.ru>
2026-06-13 22:41:50 +0300

    Добавлена физика

--- lights/src/LightsAnimation.js
+++ lights/src/LightsAnimation.js
@@ -7,7 +7,8 @@ const settings = {
         greenDiapasone: [100, 255],
         blueDiapasone: [100, 255],
     },
-    gradientStepCount: 150
+    gradientStepCount: 150,
+    forceK: 30,
 };
 
 class LightsAnimation {
@@ -45,6 +46,15 @@ class LightsAnimation {
             this.ctx.globalCompositeOperation = "lighter";
             this.draw();
         });
+
+        cnv.addEventListener('click', (e) => {
+            this.particles.push(
+                new Particle(
+                    1, e.offsetX, e.offsetY,
+                    new GradientMap(settings.gradientStepCount, settings.brightness, Color.getRandomColor(settings.color))
+                )
+            );
+        });
     }
 
     initSize () {
@@ -53,7 +63,40 @@ class LightsAnimation {
     }
 
     calc () {
+        for (let i = 0; i < this.particles.length; i++) {
+            const a = this.particles[i];
 
+            for (let j = 0; j < this.particles.length; j++) {
+                if (i === j) continue;
+                const b = this.particles[j];
+
+                const force = Vector.sub(b.position, a.position);
+                const distance = Math.min(Math.max(force.mag(), 4), 1000);
+                const strength = settings.forceK * a.m * b.m / distance ** 2;
+                force.normalize();
+                force.mult(strength / a.m);
+                a.acceleration.add(force);
+            }
+        }
+
+        this.particles.forEach(particle => {
+            particle.velocity.add(particle.acceleration);
+            particle.velocity.mult(1 - settings.friction);
+            particle.position.add(particle.velocity);
+            particle.acceleration.set(0, 0);
+
+            if (particle.position.x < 0) {
+                particle.position.x = this.cnv.width;
+            } else if (particle.position.x > this.cnv.width) {
+                particle.position.x = 0;
+            }
+
+            if (particle.position.y < 0) {
+                particle.position.y = this.cnv.height;
+            } else if (particle.position.y > this.cnv.height) {
+                particle.position.y = 0;
+            }
+        });
     }
 
     draw () {
